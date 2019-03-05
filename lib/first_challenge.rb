5bbf72eaf5a6df343e414eb3d0c2c4cdc4cf1dc6
Princeton Rose <decoyzpr@gmail.com>
2019-03-05 02:09:59 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -16,8 +16,8 @@ def first_challenge
   #your code here
   contacts.each do |name, info|
     info.each do |type, data|
-      data.each do |stuff, things|
-        puts "#{stuff} + #{things}"
+      if data == :favorite_icecream_flavors
+        :favorite_icecream_flavors.delete("strawberry")
       end
     end
   end
